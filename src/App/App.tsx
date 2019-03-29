67c49abe5d071caa694043e1155826d9d6ce8cc8
Chris Hooker <hookercj@gmail.com>
2019-03-28 20:37:50 -0700

    CSS -> SASS

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css';  
 import * as dataAccess from "../DataAccess/data-access"
 
 class App extends Component<any, any> {
