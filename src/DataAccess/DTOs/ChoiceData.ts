4be23bce0566d603456503f1cc75a8a57005ad94
Chris Hooker <hookercj@gmail.com>
2019-04-13 20:48:17 -0700

    Remove old TODOs

--- src/DataAccess/DTOs/ChoiceData.ts
+++ src/DataAccess/DTOs/ChoiceData.ts
@@ -1,9 +1,6 @@
 import { firestore } from 'firebase';
 type QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
 
-// TODO: Rename as Choice. Rename component as ChoiceView?
-// TODO: More DDD approach? Domain object and DTO?
-
 export default interface ChoiceData {
   choiceId: string;
   targetPageId: string;
