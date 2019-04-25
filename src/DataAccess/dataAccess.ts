0e26f14358e5f6f3e3b516bcc4439d831cde79b3
Chris Hooker <hookercj@gmail.com>
2019-04-24 22:34:29 -0700

    Move Firebase API key to .env

--- src/DataAccess/dataAccess.ts
+++ src/DataAccess/dataAccess.ts
@@ -1,8 +1,10 @@
 import * as firebase from "firebase";
 
+const API_KEY = process.env.FIREBASE_API_KEY;
+
 export function initialize() {
   firebase.initializeApp({
-    apiKey: ' AIzaSyBi2KbEFSMemDyHnYuKnISxWibsrMbx3fE',
+    apiKey: API_KEY,
     authDomain: 'choicebook-cjh.firebaseapp.com',
     projectId: 'choicebook-cjh'
   });
